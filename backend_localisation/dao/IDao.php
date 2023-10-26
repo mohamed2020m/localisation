3bc40f71ac201759deaae2174e059fc52801608a
leeuw <60801395+mohamed2020m@users.noreply.github.com>
2023-10-26 03:17:11 +0100

    Delete backend_localisation directory

--- backend_localisation/dao/IDao.php
+++
@@ -1,9 +0,0 @@
-<?php
-interface IDao
-{
-    public function create($obj);
-    public function update($obj);
-    public function delete($obj);
-    public function getById($obj);
-    public function getAll();
-}
--- backend_localisation/racine.php
+++
@@ -1,2 +0,0 @@
-<?php
- define("RACINE", __DIR__);
